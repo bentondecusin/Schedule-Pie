2b47b622352ebc96751c694bed5359313bfa5585
Benton Li <cl2597@cornell.edu>
2024-07-22 04:32:18 +0100

    flush first stage

---
+++ app/auth.ts
@@ -0,0 +1,80 @@
+/**
+ * The following codes are adapted from Microsoft
+ * See more at https://github.com/MicrosoftDocs/mslearn-retrieve-m365-data-with-msgraph-quickstart and adapted
+ */
+
+import "isomorphic-fetch";
+import {
+  PublicClientApplication,
+  InteractionRequiredAuthError,
+  SilentRequest,
+} from "@azure/msal-browser";
+
+//MSAL configuration
+export const msalConfig = {
+  auth: {
+    clientId: process.env.clientId || "",
+    // authority: authority ID
+    // redirectUri: "https://localhost:8080",
+  },
+};
+
+export const msalRequest = {
+  scopes: ["Calendars.read", "user.read"],
+};
+
+export function ensureScope(scope: string) {
+  if (
+    !msalRequest.scopes.some(
+      (s: string) => s.toLowerCase() === scope.toLowerCase()
+    )
+  ) {
+    msalRequest.scopes.push(scope);
+  }
+}
+
+//Initialize MSAL client
+export const msalClient: PublicClientApplication = new PublicClientApplication(
+  msalConfig
+);
+
+// Log the user in
+export async function signIn() {
+  const authResult = await msalClient
+    .initialize()
+    .then(() => msalClient.loginPopup(msalRequest));
+  sessionStorage.setItem("msalAccount", authResult.account.username);
+}
+
+//Get token from Graph
+export async function getToken() {
+  let account = sessionStorage.getItem("msalAccount");
+  if (!account) {
+    throw new Error(
+      "User info cleared from session. Please sign out and sign in again."
+    );
+  }
+  try {
+    // First, attempt to get the token silently
+    const msalAccount = msalClient.getAccountByUsername(account);
+    // If msal account does not exist
+    if (!msalAccount) throw Error("account DNE");
+
+    const silentRequest: SilentRequest = {
+      scopes: msalRequest.scopes,
+      account: msalAccount,
+    };
+    const silentResult = await msalClient.acquireTokenSilent(silentRequest);
+
+    return silentResult.accessToken;
+  } catch (silentError) {
+    // If silent requests fails with InteractionRequiredAuthError,
+    // attempt to get the token interactively
+    if (silentError instanceof InteractionRequiredAuthError) {
+      const interactiveResult = await msalClient.acquireTokenPopup(msalRequest);
+      return interactiveResult.accessToken;
+    } else {
+      throw silentError;
+    }
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,108 @@
+"use client";
+
 import Image from "next/image";
+import "./auth";
+import { Client } from "@microsoft/microsoft-graph-client";
+import "isomorphic-fetch";
+import { ensureScope, getToken, signIn } from "./auth";
+import { useState } from "react";
 
 export default function Home() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [events, setEvents] = useState([]);
+  async function displayUI() {
+    await signIn();
+    // Display info from user profile
+    await getUser().then((user) => {
+      setUserName(user && user!.displayName);
+    });
+  }
+
+  async function displayEvents() {
+    var eventsContainer = await getEvents();
+
+    setEvents(eventsContainer!.value);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {!userName && (
+        <a id="signin" href="#" onClick={displayUI}>
+          <img src="/microsoft_logo.svg" alt="Sign in with Microsoft" />
+          Sign in with Microsoft
+        </a>
+      )}
+
+      {userName && (
+        <>
+          <div id="content" className="none;">
+            <h4>
+              Welcome <span id="userName"> {userName} </span>
+            </h4>
+          </div>
+          {!events && (
+            <button id="btnShowEvents" onClick={displayEvents}>
+              Show events
+            </button>
+          )}
+
+          <div id="eventWrapper">
+            {!events && <p>Failed to retrieve from Microsoft Graph</p>}
+            {events && events.length > 0 && (
+              <>
+                <p>
+                  Your events retrieved from Microsoft Graph for the day week:
+                </p>
+                <ul>
+                  {events.map((elm: any) => (
+                    <li id="events">{`${elm.subject} - From  ${new Date(
+                      elm.start.dateTime
+                    ).toLocaleString()} to ${new Date(
+                      elm.end.dateTime
+                    ).toLocaleString()} `}</li>
+                  ))}
+                </ul>
+              </>
+            )}
+            {(!events || events.length < 1) && (
+              <p>No events retrieved from Microsoft Graph</p>
+            )}
+          </div>
+        </>
+      )}
+    </main>
   );
+}
+
+// Create an authentication provider
+const authProvider = {
+  getAccessToken: async () => {
+    // Call getToken in auth.js
+    return await getToken();
+  },
+};
+
+// Initialize the Graph client
+const graphClient: Client = Client.initWithMiddleware({ authProvider });
+
+//Get user info from Graph
+async function getUser() {
+  ensureScope("user.read");
+  return await graphClient.api("/me").select("id,displayName").get();
+}
+
+async function getEvents(): Promise<any> {
+  ensureScope("Calendars.read");
+  const dateNow = new Date();
+  const dateNextWeek = new Date();
+  dateNextWeek.setDate(dateNextWeek.getDate() + 1);
+  const query = `startDateTime=${dateNow.toISOString()}&endDateTime=${dateNextWeek.toISOString()}`;
+
+  return await graphClient
+    .api("/me/calendarView")
+    .query(query)
+    .select("subject,start,end")
+    .orderby(`start/DateTime`)
+    .get();
 }
